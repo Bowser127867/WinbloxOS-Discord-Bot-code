5cd4c8979346ec0f3f2365ecf6b77cb404c04c51
Bowser127867 <sabriomerakdeniz@gmail.com>
2024-11-09 12:42:23 +0300

    changed the code

--- main.py
+++ main.py
@@ -14,7 +14,6 @@ TOKEN = os.environ.get("DISCORD_TOKEN")
 
 import discord
 import time
-import random
 from discord.ext import commands
 
 izinler = discord.Intents.default()
@@ -26,6 +25,14 @@ bot = commands.Bot(command_prefix="!",intents= izinler)
 async def on_ready():
     print(f"{bot.user.name} has been started...Installing the codes from the WinbloxOS Servers...")
     time.sleep(1)
+    print("[=           ]%25")
+    time.sleep(1)
+    print("[===         ]%50")
+    time.sleep(1)
+    print("[========    ]%75")
+    time.sleep(1)
+    print("[============]%100")
+    time.sleep(0.1)
     print("Done!")
     
 
@@ -37,9 +44,38 @@ async def on_message(message):
         await message.channel.send(f"hello there!")
     if message.content.startswith("winbloxOS"):
         await message.channel.send(f"what?")
+    if message.content.startswith("who are you winbloxOS?"):
+        await message.channel.send(f"I am a bot created by WinbloxOS(a gaming group).They came here with one purpose...To serve quality.")
     if message.content.startswith("boo"):
         await message.channel.send(f"not loud enough.")
-    elif message.content.startswith("BOO!") or message.content.startswith("boo!"):
+    elif message.content.startswith("BOO!"):
         await message.channel.send(f"AAAAA---*dies*")
+    await bot.process_commands(message)
+
+import random
+
+@bot.command("dice")
+async def zar_at(ctx):
+    await ctx.send(str(random.randint(1,6)))
+
+@bot.command("addition")
+async def topla(ctx, sayi1,sayi2):
+    sonuc = int(sayi1) + int(sayi2)
+    await ctx.send(str(sonuc))
+
+meme_list = os.listdir("./images")
+
+@bot.command("meme")
+async def meme(ctx):
+    meme = random.choice(meme_list)
+    meme_path = os.path.join("./images",meme)
+    with open(meme_path, "rb") as img:
+        picture = discord.File(img)
+    await ctx.send(file = picture)
+
+@bot.command("subtract")
+async def subtract(ctx, sayi1,sayi2):
+    sonuc = int(sayi1) - int(sayi2)
+    await ctx.send(str(sonuc))
 
 bot.run(TOKEN)
