235f3689c15accd69e138c2412daf32af93400ce
Bowser127867 <sabriomerakdeniz@gmail.com>
2024-11-13 21:00:24 +0300

    [UPDATE:BAN AND DUCK]

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ load_dotenv(override=True)
 #THE TOKEN GETS THE DISCORD TOKEN IN THE .env FILE!!!
 TOKEN = os.environ.get("DISCORD_TOKEN")
 
+import requests
 import discord
 import time
 from discord.ext import commands
@@ -21,10 +22,20 @@ izinler.message_content = True
 
 bot = commands.Bot(command_prefix="!",intents= izinler)
 
+def get_duck_image_url():    
+    url = 'https://random-d.uk/api/random'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+
 @bot.event
 async def on_ready():
     print(f"{bot.user.name} has been started...Installing the codes from the WinbloxOS Servers...")
     time.sleep(1)
+    print("Installing WinbloxOS Code Installer...")
+    time.sleep(1)
+    print("Welcome to WinbloxOS Bot:Source Code Installer...Installing the Source code...")
+    time.sleep(3)
     print("[=           ]%25")
     time.sleep(1)
     print("[===         ]%50")
@@ -32,6 +43,12 @@ async def on_ready():
     print("[========    ]%75")
     time.sleep(1)
     print("[============]%100")
+    time.sleep(1)
+    print("Finalizing installing WinbloxOS Bot:Source Code...")
+    time.sleep(1)
+    print("Done!")
+    time.sleep(1)
+    print("Uninstalling WinbloxOS Code Setup...")
     time.sleep(0.1)
     print("Done!")
     
@@ -77,5 +94,31 @@ async def meme(ctx):
 async def subtract(ctx, sayi1,sayi2):
     sonuc = int(sayi1) - int(sayi2)
     await ctx.send(str(sonuc))
+
+@bot.command("multiply")
+async def multiply(ctx, sayi1,sayi2):
+    sonuc = int(sayi1) * int(sayi2)
+    await ctx.send(str(sonuc))
+
+@bot.command("division")
+async def division(ctx, sayi1,sayi2):
+    sonuc = int(sayi1) / int(sayi2)
+    await ctx.send(str(sonuc))
+
+@bot.command('duck')
+async def duck(ctx):
+    image_url = get_duck_image_url()
+    await ctx.send(image_url)
+
+@bot.command("banuser")
+@commands.has_permissions(ban_members=True)
+async def ban(ctx, member: discord.Member, *, reason = None):
+    try:
+        await member.ban(reason=reason)
+        await ctx.send(f'User {member.mention} has been banned for: {reason}')
+    except discord.Forbidden:
+        await ctx.send('ERR[CANTBAN]')
+    except discord.HTTPException as e:
+        await ctx.send(f'ERR[FAILEDBC: {e}]')
 
 bot.run(TOKEN)
